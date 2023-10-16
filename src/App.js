7242345716ddffb14b971346ee012aee5e06728f
Edil Zholbolduev <tashtan841@gmail.com>
2023-10-17 00:25:14 +0200

    started learning react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "./components/button"
+import { Input } from './components/input'
+
+function App() {
+  return (
+    <div className="App">
+      <Input />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+import './button.css'
+
+export function Button(params){
+   return (
+      <button className='btn'> click! </button>
+   )
+}
